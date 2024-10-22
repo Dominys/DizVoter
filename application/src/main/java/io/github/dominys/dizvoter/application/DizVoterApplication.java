86f28e77a2f908744aaed1c1fc24a8d81154da0e
roman.bezpalko <roman.bezpalko@openbet.com>
2024-10-22 01:12:05 +0100

    Initial structure

---
+++ application/src/main/java/io/github/dominys/dizvoter/application/DizVoterApplication.java
@@ -0,0 +1,13 @@
+package io.github.dominys.dizvoter.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DizVoterApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DizVoterApplication.class, args);
+  }
+
+}
---
+++ application/src/test/java/io/github/dominys/dizvoter/application/DizVoterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.dominys.dizvoter.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DizVoterApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
